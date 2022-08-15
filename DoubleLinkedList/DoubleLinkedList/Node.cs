10c5737411ffc283e5e6495295073386ac86290e
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-15 14:08:36 +0430

    Add files via upload

---
+++ DoubleLinkedList/DoubleLinkedList/Node.cs
@@ -0,0 +1,147 @@
+﻿namespace DoubleLinkedList
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node prev;
+    }
+
+    public class Operation
+    {
+        Node head = null;
+
+        public bool IsNull()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void InsertAtFirst(int value)
+        {
+            if (IsNull())
+            {
+                Node newNode = new Node();
+
+                newNode.value = value;
+                newNode.next = null;
+                newNode.prev = null;
+
+                head = newNode;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.value = value;
+
+                newNode.next = head;
+                newNode.prev = null;
+                head.prev = newNode;
+
+                head = newNode;
+            }
+        }
+
+        public void InsertAtLast(int value)
+        {
+            if (IsNull())
+            {
+                Node newNode = new Node();
+
+                newNode.value = value;
+                newNode.next = null;
+                newNode.prev = null;
+
+                head = newNode;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.value = value;
+
+                Node lastFoundNode = head;
+                while (lastFoundNode.next != null)
+                {
+                    lastFoundNode = lastFoundNode.next;
+                }
+
+                newNode.prev = lastFoundNode;
+                newNode.next = null;
+                lastFoundNode.next = newNode;
+            }
+        }
+
+        public void DeleteFromLast() 
+        {
+            if (!IsNull())
+            {
+                Node lastNode = head;
+
+                while (lastNode.next != null)
+                {
+                    lastNode = lastNode.next;
+
+                }
+
+                if (lastNode.prev == null)
+                {
+                    head = null; 
+                }
+                else
+                {
+                    lastNode.prev.next = null;
+                }
+            }
+            else
+            {
+                Console.WriteLine("there is nothing in order to delete!");
+            }
+        }
+
+        public void DeleteFromFirst()
+        {
+            if (!IsNull())
+            {
+                Node tempNode = head.next;
+                head = null;
+                head = tempNode;
+            }
+            else
+            {
+                Console.WriteLine("there is nothing in order to delete!");
+            }
+        }
+
+        public void ClearList()     
+        {
+            if (!IsNull()) 
+            {
+                head = null;
+            }
+            else
+            {
+                Console.WriteLine("there is nothing in order to delete!");
+            }
+        }
+
+        public void Print()
+        {
+            if (!IsNull())
+            {
+                Node tempNode = head;
+                while (tempNode != null)
+                {
+                    Console.WriteLine(tempNode.value);
+                    tempNode = tempNode.next;
+                }
+            }
+        }
+    }
+}
---
+++ DoubleLinkedList/DoubleLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DoubleLinkedList;
+
+Operation x = new Operation();
+x.InsertAtFirst(2);
+x.InsertAtFirst(1);
+x.DeleteFromFirst();
+x.InsertAtLast(3);
+x.InsertAtLast(4);
+x.InsertAtLast(5);
+x.InsertAtLast(6);
+x.InsertAtFirst(0);
+x.DeleteFromFirst();
+
+    
+x.Print();
+

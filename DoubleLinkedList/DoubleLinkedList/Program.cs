6edf177663ef3e3351d087bd38cd97c86190eeb4
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-15 16:45:49 +0430

    Update Program.cs

--- DoubleLinkedList/DoubleLinkedList/Program.cs
+++ DoubleLinkedList/DoubleLinkedList/Program.cs
@@ -1,17 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DoubleLinkedList;
 
-Operation x = new Operation();
+LinkedListOperation x = new LinkedListOperation();
 x.InsertAtFirst(2);
 x.InsertAtFirst(1);
-x.DeleteFromFirst();
 x.InsertAtLast(3);
 x.InsertAtLast(4);
 x.InsertAtLast(5);
 x.InsertAtLast(6);
-x.InsertAtFirst(0);
-x.DeleteFromFirst();
 
-    
+x.InsertAfter(6, 200);
+x.InsertAfter(200, 86);
+
+x.DeleteFromMiddle(86);
+
 x.Print();
+
 
